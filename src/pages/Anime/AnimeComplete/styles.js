7d03c5b5d86f06831590adbc39235cef59f3746c
Hakeem <hakeemsbuchanan@outlook.com>
2023-02-04 19:03:26 -0500

    Style update and box flex

--- src/pages/Anime/AnimeComplete/styles.js
+++ src/pages/Anime/AnimeComplete/styles.js
@@ -2,9 +2,13 @@ import styled from 'styled-components'
 import { white } from '@carbon/colors'
 
 export const AnimeItem = styled.li`
+.box{
+  
   margin-top: .5rem;
   padding-bottom: 2rem;
-  border-bottom: 2px solid ${white};
+  border-style:solid;
+}
+border-bottom: 1px solid ${white};
 `
 
 export const AnimeName = styled.h4`
@@ -13,12 +17,15 @@ export const AnimeName = styled.h4`
 `
 
 export const AnimeDescription = styled.p`
+.box{
+  display: flex;
   font-weight: bold;
   padding-top:60px;
   width:auto;
   height: 40px;
   text-align:bottom;
- 
+  word-wrap: break-word;
+  }
   
 `
 
--- src/pages/Anime/AnimeFave/styles.js
+++ src/pages/Anime/AnimeFave/styles.js
@@ -2,23 +2,31 @@ import styled from 'styled-components'
 import { white } from '@carbon/colors'
 
 export const AnimeItem = styled.li`
+.box{
+  
   margin-top: .5rem;
   padding-bottom: 2rem;
-  border-bottom: 1px solid ${white};
+  border-style:solid;
+}
+border-bottom: 1px solid ${white};
 `
 
 export const AnimeName = styled.h4`
   font-weight: bold;
   text-align:left;
+  display: flex;
 `
 
 export const AnimeDescription = styled.p`
+  .box{
+  display: flex;
   font-weight: bold;
   padding-top:60px;
   width:auto;
   height: 40px;
   text-align:bottom;
- 
+  word-wrap: break-word;
+  }
   
 `
 
